60e456907a8ae7ca7588dbcd0aa5bf7ce40a90ce
Francisco Costa <francisco100eg@gmail.com>
2022-09-18 21:16:35 -0300

    corrigir erro

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -35,7 +35,7 @@ const Header = () => {
             </div>
           </div>
           <div className='tag-cmp app__flex'>
-            <p className='p-text'>Web Developer</p>
+            <p className='p-text'>Desenvolvedor Web</p>
             <p className='p-text'>UI/UX Design</p>
           </div>
         </div>
